37600284014cff7f0ebfbfe1c9e83f8aa44d12d3
yijunjun <junjunyi@gmail.com>
2018-05-31 20:17:18 +0800

    去除多余的依赖

--- main.go
+++ main.go
@@ -2,13 +2,43 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"path"
+	"runtime"
+	"strings"
 
-	"dw7758.com/common"
-	"dw7758.com/common/config"
 	_ "github.com/go-sql-driver/mysql"
 )
+
+func IsIn(target string, eles []string) bool {
+	for _, ele := range eles {
+		if target == ele {
+			return true
+		}
+	}
+	return false
+}
+
+func NewStackErr(reason string) error {
+	pc, file, line, ok := runtime.Caller(1)
+	if !ok {
+		return errors.New(reason)
+	}
+	fun := runtime.FuncForPC(pc)
+	return fmt.Errorf(
+		"[%v %v:%v] %v",
+		path.Base(file),
+		fun.Name(),
+		line,
+		reason,
+	)
+}
 
 type Columns struct {
 	Name         string
@@ -30,7 +60,12 @@ func (this *Columns) ToString() {
 }
 
 type MySqlServer struct {
-	*sql.DB
+	*sql.DB  `json:"-"`
+	Title    string
+	Host     string
+	Port     int32
+	User     string
+	Password string
 }
 
 /*
@@ -70,7 +105,7 @@ func (this *MySqlServer) ListColumns(table string) ([]*Columns, error) {
 	`, table)
 	rows, err := this.Query(sql)
 	if err != nil {
-		return nil, common.NewStackErr(err.Error())
+		return nil, NewStackErr(err.Error())
 	}
 	defer rows.Close()
 
@@ -84,13 +119,13 @@ func (this *MySqlServer) ListColumns(table string) ([]*Columns, error) {
 			&col.Key,
 		)
 		if err != nil {
-			return nil, common.NewStackErr(err.Error())
+			return nil, NewStackErr(err.Error())
 		}
 		cols = append(cols, col)
 	}
 	err = rows.Err()
 	if err != nil {
-		return nil, common.NewStackErr(err.Error())
+		return nil, NewStackErr(err.Error())
 	}
 
 	return cols, nil
@@ -99,7 +134,7 @@ func (this *MySqlServer) ListColumns(table string) ([]*Columns, error) {
 func (this *MySqlServer) ListTables() ([]string, error) {
 	rows, err := this.Query("select distinct table_name from information_schema.columns where table_schema='dw'")
 	if err != nil {
-		return nil, common.NewStackErr(err.Error())
+		return nil, NewStackErr(err.Error())
 	}
 	defer rows.Close()
 
@@ -108,67 +143,97 @@ func (this *MySqlServer) ListTables() ([]string, error) {
 		var name string
 		err = rows.Scan(&name)
 		if err != nil {
-			return nil, common.NewStackErr(err.Error())
+			return nil, NewStackErr(err.Error())
 		}
 		names = append(names, name)
 	}
 	err = rows.Err()
 	if err != nil {
-		return nil, common.NewStackErr(err.Error())
+		return nil, NewStackErr(err.Error())
 	}
 
 	return names, nil
 }
 
-func NewMySqlServer(iniconf config.Configer) (*MySqlServer, error) {
-	MysqlHost := iniconf.DefaultString("MysqlHost", "127.0.0.1")
-	MysqlPort := iniconf.DefaultInt("MysqlPort", 3306)
-	MysqlUser := iniconf.DefaultString("MysqlUser", "dw")
-	MysqlPassword := iniconf.DefaultString("MysqlPassword", "dw_wd251")
+func NewMySqlServer(jsonConf string) (*MySqlServer, error) {
+	jsonBS, err := ioutil.ReadFile(jsonConf)
+	if err != nil {
+		return nil, NewStackErr(err.Error())
+	}
 
 	mss := &MySqlServer{}
 
-	var err error
-	mss.DB, err = sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/?charset=utf8",
-		MysqlUser,
-		MysqlPassword,
-		MysqlHost,
-		MysqlPort,
-	))
+	fmt.Println(
+		string(jsonBS),
+		strings.TrimSpace(string(jsonBS)),
+		string(bytes.TrimSpace(jsonBS)),
+	)
+
+	err = json.Unmarshal(bytes.Trim(jsonBS, "\t\n"), mss)
 	if err != nil {
-		return nil, common.NewStackErr(err.Error())
+		return nil, NewStackErr(err.Error())
 	}
+
+	mss.DB, err = sql.Open(
+		"mysql",
+		fmt.Sprintf(
+			"%v:%v@tcp(%v:%v)/?charset=utf8",
+			mss.User,
+			mss.Password,
+			mss.Host,
+			mss.Port,
+		),
+	)
+	if err != nil {
+		return nil, NewStackErr(err.Error())
+	}
+
 	err = mss.Ping()
 	if err != nil {
-		return nil, common.NewStackErr(err.Error())
+		return nil, NewStackErr(err.Error())
 	}
 	return mss, nil
 }
 
-func LoadConfig() error {
-	leftConf, err := common.LoadIniConfig("left.conf")
+func LoadConfig(leftJosn, rightJosn string) error {
+	leftServer, err := NewMySqlServer(leftJosn)
 	if err != nil {
-		return common.NewStackErr(err.Error())
+		return NewStackErr(err.Error())
 	}
 
-	leftServer, err := NewMySqlServer(leftConf)
+	rightServer, err := NewMySqlServer(rightJosn)
 	if err != nil {
-		return common.NewStackErr(err.Error())
+		return NewStackErr(err.Error())
 	}
 
-	rightConf, err := common.LoadIniConfig("right.conf")
+	// 以左边为标准
+	leftTables, err := leftServer.ListTables()
 	if err != nil {
-		return common.NewStackErr(err.Error())
+		return NewStackErr(err.Error())
 	}
 
-	rightServer, err := NewMySqlServer(rightConf)
+	rightTables, err := rightServer.ListTables()
 	if err != nil {
-		return common.NewStackErr(err.Error())
+		return NewStackErr(err.Error())
 	}
+
+	for _, rt := range rightTables {
+		if !IsIn(rt, leftTables) {
+			fmt.Println("delete", rt)
+		}
+	}
+
+	for _, lt := range leftTables {
+		if !IsIn(lt, rightTables) {
+			fmt.Println("add", lt)
+		}
+	}
+
+	// 取公共
 
 	cols, err := leftServer.ListColumns("dw_user")
 	if err != nil {
-		return common.NewStackErr(err.Error())
+		return NewStackErr(err.Error())
 	}
 
 	for _, col := range cols {
@@ -177,28 +242,24 @@ func LoadConfig() error {
 
 	_, err = rightServer.ListColumns("dw_user")
 	if err != nil {
-		return common.NewStackErr(err.Error())
+		return NewStackErr(err.Error())
 	}
 
 	return nil
-
-	//	leftTables, err := leftServer.ListTables()
-	//	if err != nil {
-	//		common.ErrorWithStack(err.Error())
-	//		return
-	//	}
-
-	//	rightTables, err := rightServer.ListTables()
-	//	if err != nil {
-	//		common.ErrorWithStack(err.Error())
-	//		return
-	//	}
-
-	//	fmt.Println("leftTables:", leftTables)
-
-	//	fmt.Println("rightTables:", rightTables)
 }
 
 func main() {
-	fmt.Println("err:", LoadConfig())
+	var leftJson, rightJson string
+
+	flag.StringVar(&leftJson, "leftJson", "left.json", "left json conf")
+	flag.StringVar(&rightJson, "rightJson", "right.json", "right json conf")
+
+	flag.Parse()
+
+	fmt.Println(fmt.Sprintf(
+		"leftJson:%v, rightJson:%v",
+		leftJson, rightJson,
+	))
+
+	fmt.Println(LoadConfig(leftJson, rightJson))
 }
